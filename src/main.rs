7a336eba37da11623efe5b7eb32a9e8906230d7e
Elias Rammos <rammoselias@gmail.com>
2025-09-10 09:13:46 +0200

    initial

--- src/main.rs
+++ src/main.rs
@@ -16,17 +16,21 @@ impl Timer {
 #[macroquad::main("BasicShapes")]
 async fn main() {
     let mut history: Vec<String> = Vec::new();
+    let mut history_index = 0;
     let mut command = String::from("");
     let mut status = String::from("#");
     let mut time_cursor = Timer::now();
     let mut time_del = Timer::now();
     let mut toggle = false;
+    const initial_y: f32 = 100.0;
 
     loop {
         clear_background(BLACK);
 
         match get_char_pressed() {
-            Some(key) if key.is_ascii_whitespace() || key.is_ascii_alphanumeric() => {
+            Some(key)
+                if (key.is_alphanumeric() || key.is_whitespace()) && key != '\r' && key != '\n' =>
+            {
                 command.push(key);
             }
             _ => (),
@@ -41,13 +45,16 @@ async fn main() {
             command.pop();
         }
         let mut offset = 0.0;
-        for text in &history {
-            let rect = draw_text(text, 0.0, 100.0 + offset, 30.0, GREEN);
+        if history.len() - history_index > 20 {
+            history_index += 1;
+        }
+        for text in &history[history_index..] {
+            let rect = draw_text(text, 0.0, initial_y + offset, 30.0, GREEN);
             offset += rect.height + 2.0;
         }
 
-        let status_rect = draw_text(&status, 0.0, offset + 100.0, 30.0, GREEN);
-        let cmd_rect = draw_text(&command, status_rect.width, offset + 100.0, 30.0, GREEN);
+        let status_rect = draw_text(&status, 0.0, initial_y + offset, 30.0, GREEN);
+        let cmd_rect = draw_text(&command, status_rect.width, initial_y + offset, 30.0, GREEN);
 
         if time_cursor.elapsed() > 500 && !toggle {
             toggle = true;
@@ -62,7 +69,7 @@ async fn main() {
             draw_text(
                 "|",
                 cmd_rect.width + status_rect.width,
-                offset + 100.0,
+                initial_y + offset,
                 30.0,
                 GREEN,
             );
